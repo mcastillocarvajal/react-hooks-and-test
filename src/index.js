9a5b8c1402c21b16ae689be7778e4737c24d5810
Moises <mcastillo093@gmail.com>
2021-06-09 15:43:36 -0600

    useContext

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import { CallbackHook } from './components/06-Memos/CallbackHook';
 // import { Padre } from './components/07-tarea-memo/Padre';
-import { TodoApp } from './components/08-useReducer/TodoApp';
+// import { TodoApp } from './components/08-useReducer/TodoApp';
+import { MainApp } from './components/09-useContext/MainApp';
 // import { MemoHook } from './components/06-Memos/MemoHook';
 // import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
 // import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
@@ -16,7 +17,7 @@ import { TodoApp } from './components/08-useReducer/TodoApp';
 // import { HooksApp } from './hooks-app';
 
 ReactDOM.render(
-  <TodoApp />,
+  <MainApp />,
   document.getElementById('root')
 );
 
