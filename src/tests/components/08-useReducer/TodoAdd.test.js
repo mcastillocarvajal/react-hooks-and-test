7270d753302032441a5dd3f86908ce8bc4b6a019
Moises <mcastillo093@gmail.com>
2021-06-11 15:22:50 -0600

    test for useReducer completed

--- src/tests/components/08-useReducer/TodoAdd.test.js
+++ src/tests/components/08-useReducer/TodoAdd.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { TodoAdd } from '../../../components/08-useReducer/TodoAdd';
-import { demoTodos } from '../../fixtures/demoTodos';
 
 describe('test for <TodoAdd />', () => {
 
@@ -28,8 +27,27 @@ describe('test for <TodoAdd />', () => {
     })
 
     test('should call function handleAddTodo', () => {
-        
+
+        const value = 'Learn Node';
+        wrapper.find('input').simulate('change', {
+            target: {
+                value,
+                name: 'description'
+            }
+        })
+
+        const formSubmit = wrapper.find('form').prop('onSubmit');
+
+        formSubmit({ preventDefault(){} });
+
+        expect( handleAddTodo ).toHaveBeenCalledTimes(1);
+        expect( handleAddTodo ).toHaveBeenCalledWith( expect.any(Object) );
+        expect( handleAddTodo ).toHaveBeenCalledWith({
+            id: expect.any(Number),
+            desc: value,
+            done: false
+        });
+        expect( wrapper.find('input').prop('value')).toBe('');
     })
-    
-    
+
 })
---
+++ src/tests/components/08-useReducer/TodoApp.test.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import { TodoApp } from '../../../components/08-useReducer/TodoApp';
+import { demoTodos } from '../../fixtures/demoTodos';
+import { act } from '@testing-library/react';
+
+describe('Test for <TodoApp />', () => {
+
+    const wrapper = shallow( <TodoApp />);
+
+    Storage.prototype.setItem = jest.fn();
+
+    test('should show component correctly', () => {
+        
+        expect( wrapper ).toMatchSnapshot();
+    })
+    
+    test('should add a todo', () => {
+        
+        const wrapper = mount( <TodoApp />);
+
+        act( () => {
+            
+            wrapper.find('TodoAdd').prop('handleAddTodo')( demoTodos[0] );
+            wrapper.find('TodoAdd').prop('handleAddTodo')( demoTodos[1] );
+        });
+
+        expect( wrapper.find('h1').text().trim() ).toBe('TodoApp  ( 2 )')
+        expect ( localStorage.setItem ).toHaveBeenCalledTimes(2);
+    })
+
+    test('should delete a todo', () => {
+
+        wrapper.find('TodoAdd').prop('handleAddTodo')( demoTodos[0] );
+        wrapper.find('TodoList').prop('handleDelete')( demoTodos[0].id );
+
+        expect( wrapper.find('h1').text().trim() ).toBe('TodoApp  ( 0 )')
+    })
+    
+    
+})
